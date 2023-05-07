29f2eee8e7ea3cc6a256b30622b5a0a34c58e558
alihasanarslan <73365938+alihasanarslan@users.noreply.github.com>
2023-05-07 17:21:34 +0300

    Update generate.js

--- public/generate.js
+++ public/generate.js
@@ -1,1 +1,5 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("generateButton").addEventListener("click", function() {
+    window.open("https://go.dev/play/p/7ltvaj8lhRl", "_blank");
+  });
+});
