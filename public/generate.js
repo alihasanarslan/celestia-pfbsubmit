07abbcc643c2633275d2745ab291f368434163ad
alihasanarslan <73365938+alihasanarslan@users.noreply.github.com>
2023-05-07 17:16:09 +0300

    Delete generate.js

--- public/generate.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("generateButton").addEventListener("click", function() {
-    window.open("https://go.dev/play/p/7ltvaj8lhRl", "_blank");
-  });
-});
