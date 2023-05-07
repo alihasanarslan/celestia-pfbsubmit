21c5c186b9aff7ba8c850f80440beb97223d0421
alihasanarslan <73365938+alihasanarslan@users.noreply.github.com>
2023-05-07 17:21:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("generateButton").addEventListener("click", function() {
-    window.open("https://go.dev/play/p/7ltvaj8lhRl", "_blank");
-  });
-});
 
 const { exec } = require('child_process');
 const express = require('express');
