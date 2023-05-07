5b54520ef8e01cb9e84e3b8fdac1ae56a1750b14
alihasanarslan <alihasanarslan289@gmail.com>
2023-05-07 15:51:35 +0300

    last

---
+++ public/generate.js
@@ -0,0 +1,17 @@
+function generate() {
+
+
+    document.getElementById("generateButton").addEventListener("click", function() {
+      window.open("https://go.dev/play/p/7ltvaj8lhRl", "_blank");
+  });
+  
+  const namespace_id = document.getElementById("namespace_id").value;
+  const message = document.getElementById("message").value;
+  if (namespaceId && message) {
+    const result = document.getElementById("result");
+    result.innerHTML = `Namespace ID: ${namespace_id} <br> Mesaj: ${message}`;
+  } else {
+    alert("Please fill in all fields.");
+  }
+    
+    }
