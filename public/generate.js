f4d4ff0d68ba90a5ca1a6525c13baafa39ea0890
alihasanarslan <73365938+alihasanarslan@users.noreply.github.com>
2023-05-07 16:13:39 +0300

    Update generate.js

--- public/generate.js
+++ public/generate.js
@@ -1,14 +1,14 @@
 function generate() {
 
 
-    document.getElementById("generateButton").addEventListener("click", function() {
-      window.open("https://go.dev/play/p/7ltvaj8lhRl", "_blank");
-  });
-  
-  const namespace_id;
-   document.getElementById("namespace_id").value=namespace_id;
-  const message;
-   document.getElementById("message").value=message;
+  document.getElementById("generateButton").addEventListener("click", function() {
+    window.open("https://go.dev/play/p/7ltvaj8lhRl", "_blank");
+});
 
-    
-    }
+var namespace_id;
+ document.getElementById("namespace_id").value=namespace_id;
+var message;
+ document.getElementById("message").value=message;
+
+  
+  }
